7a3fe6742b936b83015ce4e367be58b320fab840
basilcea <19818425+basilcea@users.noreply.github.com>
2026-05-03 16:54:22 -0300

    update pwa

---
+++ shoppa_pwa/public/sw.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = 'shoppa-v1'
+const OFFLINE_URL = '/offline.html'
+
+const PRECACHE_URLS = [
+  '/',
+  '/app',
+  '/app/',
+  '/manifest.json',
+  '/offline.html',
+]
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(PRECACHE_URLS)
+    })
+  )
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName)
+          }
+        })
+      )
+    })
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request).catch(() => {
+        return caches.match(OFFLINE_URL)
+      })
+    )
+  } else {
+    event.respondWith(
+      caches.match(event.request).then((cachedResponse) => {
+        return cachedResponse || fetch(event.request)
+      })
+    )
+  }
+})
